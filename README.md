2ffbf44fc0256fdda18e70408d9760fb5767c676
crserran <criserrano93@gmail.com>
2020-05-24 23:34:30 +0200

    Media service force stop when stop_delay

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # HOME ALARM
+
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/crserran/home-alarm?style=for-the-badge) 
+![GitHub Release Date](https://img.shields.io/github/release-date/maykar/custom-header?style=for-the-badge) 
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=for-the-badge)](https://github.com/hacs/integration)
+[![buy-me-a-beer](https://img.shields.io/badge/sponsor-Buy%20me%20a%20beer-orange?style=for-the-badge)](https://www.buymeacoffee.com/crserran)
+
 Home Alarm allows you to use any media player such as Alexa or Google Home to make them act as an alarm system at home. It also supports the notification system to alert you if you are not at home.
 
 # Documentation
 This security system keep listening the window and door sensors that are installed in your smart home system and the safe mode input.
+
 If the safe mode is active and some of windows or doors are opened, the user has a few seconds to indentify himself into the system. ([see link](https://www.github.com/crserran/))
+
 If the user cannot identify himself into the system, the alarm will be fired on the media player and the user will be notified that an unathorized user has been entered to his house.
 
 You can check the full documentation [here](https://www.github.com/crserran/)
 
-# Example 
+
+# Example
+
 This is a quick example about how to configure the application with Alexa as media player and 2 window/door sensors.
 
 ```yaml
--- apps/home_alarm/home_alarm.py
+++ apps/home_alarm/home_alarm.py
@@ -1,6 +1,18 @@
 import hassapi as hass
 
+
 class HomeAlarm(hass.Hass):
+  MEDIA_PLAYER_PLAY = "media_player/play_media"
+  MEDIA_PLAYER_STOP = "media_player/media_stop"
+  MEDIA_PLAYER_SET_VOL = "media_player/volume_set"
+  MEDIA_PLAYER_CTYPE = "sound"
+  MEDIA_PLAYER_VOLUME = 0.1
+  ACTIVATION_DELAY = 15
+  STOP_DELAY = 180
+  NOTIFICATION_TITLE = "ALERT!"
+  NOTIFICATION_MSG = "Alarm has been fired!"
+  ON = "on"
+  OFF = "off"
 
   def initialize(self) -> None: 
     self.log("Welcome to Home Alarm security system.")
@@ -9,18 +21,18 @@ class HomeAlarm(hass.Hass):
     self.media_player = self.args["media_player"]
     self.sound = self.args["sound"]
     self.notifiers = self.get_notifiers(self.args["notifiers"])
-    self.activation_delay = self.args.get("activation_delay", 15)
-    self.loop_delay = self.args.get("loop_delay", 3)
-    self.stop_delay = self.args.get("stop_delay", 120)
-    self.title = self.args.get("alert_title", "ALERT!")
-    self.alert_msg = self.args.get("alert_msg", "Alarm has been fired!")
+    self.activation_delay = self.args.get("activation_delay", self.ACTIVATION_DELAY)
+    self.loop_delay = self.args.get("loop_delay", None)
+    self.stop_delay = self.args.get("stop_delay", self.STOP_DELAY)
+    self.title = self.args.get("alert_title", self.NOTIFICATION_TITLE)
+    self.alert_msg = self.args.get("alert_msg", self.NOTIFICATION_MSG)
     self.alarm_is_running = False
     self.alarm_is_about_to_run = False # Alarm is triggered. Avoid multiple calls to countdown function
     self.sensor_fired_name = ""
 
-    self.listen_state(self.disarm_alarm, self.safe_mode, new="off")
+    self.listen_state(self.disarm_alarm, self.safe_mode, new=self.OFF)
     for sensor in self.sensors:
-      self.listen_state(self.door_opened_cb, sensor, new="on")
+      self.listen_state(self.door_opened_cb, sensor, new=self.ON)
 
   def door_opened_cb(self, sensor, attribute, old, new, kwargs):  
     self.sensor_fired_name = self.friendly_name(sensor)
@@ -29,7 +41,7 @@ class HomeAlarm(hass.Hass):
     self.log(f"`alarm_is_about_to_run`: {self.alarm_is_about_to_run}")
     self.log(f"`alarm_is_running`: {self.alarm_is_running}")
     self.log(f"`safe_mode` state: {safe_mode_state}")
-    if (safe_mode_state == "on" 
+    if (safe_mode_state == self.ON
         and not self.alarm_is_about_to_run
         and not self.alarm_is_running):
       self.alarm_is_about_to_run = True
@@ -37,11 +49,11 @@ class HomeAlarm(hass.Hass):
 
   def countdown(self, kwargs):
     safe_mode_state = self.get_state(self.safe_mode)
-    if safe_mode_state == "on":
+    if safe_mode_state == self.ON:
       self.call_service(
-        "media_player/volume_set", 
+        self.MEDIA_PLAYER_SET_VOL, 
         entity_id=self.media_player,
-        volume_level=1
+        volume_level=self.MEDIA_PLAYER_VOLUME
       )
       self.log("The alarm has been triggered")
       self.alarm_is_running = True
@@ -59,22 +71,31 @@ class HomeAlarm(hass.Hass):
   def fire_alarm(self, kwargs=None):
     if self.alarm_is_running:
       self.call_service(
-        "media_player/play_media",
+        self.MEDIA_PLAYER_PLAY,
         entity_id=self.media_player,
         media_content_id=self.sound,
-        media_content_type="sound"
+        media_content_type=self.MEDIA_PLAYER_CTYPE
       )
-      self.run_in(self.fire_alarm, self.loop_delay)
+      if self.loop_delay:
+        self.run_in(self.fire_alarm, self.loop_delay)
     else:
       self.log("The alarm has been stopped")
 
   def stop_alarm(self, kwargs=None):
-    self.alarm_is_running = False
+    self.stop_media_service()
 
   def disarm_alarm(self, safe_mode, attribute, old, new, kwargs=None):
-    self.log("Alarm has been disarmed")
     if self.alarm_is_running:
-      self.alarm_is_running = False
+      self.log("Alarm has been disarmed")
+      self.stop_media_service()
+
+  def stop_media_service(self):
+    self.log("Stopping media service...")
+    self.call_service(
+      self.MEDIA_PLAYER_STOP,
+      entity_id=self.media_player
+    )
+    self.alarm_is_running = False
 
   def get_notifiers(self, notifiers):
     return [n.replace('.', '/') for n in notifiers]
