365ca9828e9309fa4fb74f9de5a6c8885878f5af
nvk <nvk@MBP-de-nvk.lan>
2023-01-22 12:15:54 +0100

    File formatted

--- apps/home_alarm/home_alarm.py
+++ apps/home_alarm/home_alarm.py
@@ -39,7 +39,9 @@ class HomeAlarm(hass.Hass):
 
         self.listen_state(self.safe_mode_cb, self.safe_mode)
         for sensor in self.sensors:
-            self.listen_state(self.door_opened_cb, sensor, new=Generic.ON, old=Generic.OFF)
+            self.listen_state(
+                self.door_opened_cb, sensor, new=Generic.ON, old=Generic.OFF
+            )
 
     async def safe_mode_cb(self, sensor, attribute, old, new, kwargs):
         if new == Generic.ON:
